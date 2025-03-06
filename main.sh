76cd57bac1e2f250385539f96deb074828ae45df
fp9931 <164513821+fp9931@users.noreply.github.com>
2025-03-06 09:44:00 +0100

    updatate

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --nodes=1                    # 1 node
-#SBATCH --ntasks=1
-#SBATCH --gres=gpu:1
-#SBATCH --ntasks-per-node=1          # 1 tasks per node
-#SBATCH --time=22:00:00                 # time limits: 1 hour
+#SBATCH --nodes=2                    # 1 node
+#SBATCH --ntasks=2
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks-per-node=2          # 1 tasks per node
+#SBATCH --time=24:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
 #SBATCH --error=output.err       # standard error file
 #SBATCH --output=output.out      # standard output file
